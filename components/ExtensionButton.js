7e958ef7162a76c1c1f56c7dc95eb076c7eaa756
glmn <glmnio@outlook.com>
2024-01-04 02:27:06 +0300

    save

---
+++ components/ExtensionButton.js
@@ -0,0 +1,7 @@
+export function ExtensionButton({ text }) {
+  return (
+    <span className="p-2 bg-slate-900 border text-slate-100 border-slate-800 text-sm rounded">
+      {text}
+    </span>
+  );
+}
