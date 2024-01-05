5a3fdb468265342af149a5c89f8269509555623b
glmn <glmnio@outlook.com>
2024-01-05 13:56:44 +0300

    add og tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,8 @@
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import LastUpdatedAt from "./components/LastUpdatedAt";
 import { Logo } from "./components/Logo";
 import { ProjectLogo } from "./components/ProjectLogo";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
@@ -26,6 +27,26 @@ const config: DocsThemeConfig = {
   },
   toc: {
     title: "На этой странице"
+  },
+  useNextSeoProps: () => ({titleTemplate: '%s — Нейромаркет'}),
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://docs.neuromarket.online' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Нейромаркет — Документация'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Инструкции по сервису внешней аналитики, телеграм-боту и расширению для рекламы на Wildberries'}
+        />
+        <link rel="canonical" href={url}></link>
+      </>
+    )
   },
   editLink: { text: "" },
   search: { placeholder: "Поиск по документации" },
